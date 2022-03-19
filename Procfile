8d6d8704800b6740162f0e2b7408a1df61b26507
Gadibia Daro <55360356+dizsart@users.noreply.github.com>
2022-03-19 17:26:05 +0100

    add system.properties

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dspring.profiles.active=prod -Dserver.port=$PORT -jar target/Accounts-Service-0.0.1-SNAPSHOT.jar
+web: java -Dspring.profiles.active=default -Dserver.port=$PORT -jar target/Accounts-Service-0.0.1-SNAPSHOT.jar
--- src/main/java/com/reloadly_task/accountsService/AccountsServiceApplication.java
+++ src/main/java/com/reloadly_task/accountsService/AccountsServiceApplication.java
@@ -4,10 +4,12 @@ package com.reloadly_task.accountsService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableEurekaClient
+@Profile("dev")
 public class AccountsServiceApplication {
 
     public static void main(String[] args) {
